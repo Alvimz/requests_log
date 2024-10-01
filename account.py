add09d52fa27551556299656189ecd0c1d88357f
gAlvim <ledyzito@hotmail.com>
2024-10-01 20:42:36 -0300

    account método apply transition

--- account.py
+++ account.py
@@ -3,9 +3,17 @@ class Account:
     def __init__(self) -> None:
         self.amount= 10000
     
+    #getter and setter here
     
-    def apply_transition(self,transacao):
-        self.amount -= transacao.value
+    def apply_transition(self,transacao:str):
+        match transacao:
+            case 'Crédito':
+                self.deposit()
+            case 'Débito':
+                self.withdraw()
+                
+                
+            
     
     def deposit(self,value):
         self.amount+=value
