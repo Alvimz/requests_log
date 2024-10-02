5b0dcc00a38cea3d833e4d69dceaeb3b0a7e5465
gAlvim <ledyzito@hotmail.com>
2024-10-02 13:51:41 -0300

    Criação do log

--- log.py
+++ log.py
@@ -1,9 +1,15 @@
 from unique_transition import UniqueTransition
+import os
 class Log:
     def __init__(self) -> None:
-        
         self.list_logs = list()
+        self.create_log()
     
+    @staticmethod
+    def create_log():
+        if not os.path.exists('./log.txt'):
+            with open('log.txt','w') as f:
+                ...
     
     def add_request_log(self,transition:UniqueTransition):
         self.list_logs.append(transition)
